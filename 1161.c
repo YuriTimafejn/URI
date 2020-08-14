887ec0273a43c06640742e929bc8e1dbc17b2393
Timafejn Yuri <timafejn@gmail.com>
2020-08-14 02:10:08 -0300

    1021 precisa ser refeito

--- 1161.c
+++ 1161.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 #include <stdlib.h>
 
 int main(void){
-    long long int m, n, aux1 = 1, aux2 = 1;
-
+    long long int m, n, aux1, aux2;
+    int i;
     while(scanf("%lld %lld", &m, &n) != EOF){
-        for(int i = m; i > 0; --i){
+        aux1 = 1;
+        aux2 = 1;
+        for(i = m; i > 0; --i){
             aux1 *= m;
             m--;
         }
 
-        for(int i = n; i > 0; --i){
+        for(i = n; i > 0; --i){
             aux2 *= n;
             n--;
         }
